3c689a2cc36789ce36e0e84ec88653d9cc5119a7
Roman Morgunov <intel.opus@gmail.com>
2022-12-18 04:16:00 +0400

    Implemented the basic functionality of the bot

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module relax-bot
 go 1.19
 
 require (
-	github.com/joho/godotenv v1.4.0 // indirect
-	gopkg.in/telebot.v3 v3.1.2 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/rollbar/rollbar-go v1.4.5
+	gopkg.in/telebot.v3 v3.1.2
+	gorm.io/driver/sqlite v1.4.3
+	gorm.io/gorm v1.24.2
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"github.com/rollbar/rollbar-go"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	tele "gopkg.in/telebot.v3"
@@ -10,7 +16,49 @@ import (
 
 import _ "github.com/joho/godotenv/autoload"
 
+type User struct {
+	gorm.Model
+	TgID      int64
+	Username  string     `gorm:"uniqueIndex"`
+	Keywords  []*Keyword `gorm:"many2many:user_keywords;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (u *User) Recipient() string {
+	return strconv.FormatInt(u.TgID, 10)
+}
+
+type Keyword struct {
+	gorm.Model
+	Name      string  `gorm:"uniqueIndex"`
+	Users     []*User `gorm:"many2many:user_keywords;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
 func main() {
+	rollbar.SetToken(os.Getenv("ROLLBAR_TOKEN"))
+	rollbar.SetEnvironment(os.Getenv("APP_ENV"))
+	rollbar.SetCodeVersion(os.Getenv("ROLLBAR_CODE_VERSION"))
+	rollbar.SetServerHost(os.Getenv("ROLLBAR_HOST"))
+	rollbar.SetServerRoot(os.Getenv("ROLLBAR_ROOT"))
+
+	db, err := gorm.Open(sqlite.Open("main.db"), &gorm.Config{})
+	if err != nil {
+		rollbar.LogPanic(err, true)
+		panic(errors.New("failed to connect database"))
+	}
+
+	err = db.AutoMigrate(&User{}, &Keyword{})
+	if err != nil {
+		rollbar.LogPanic(err, true)
+		panic(errors.New("failed to auto migrate database"))
+	}
+
+	var keywords = []Keyword{{Name: "#го"}, {Name: "#знакомство"}, {Name: "#рекомендую"}}
+	db.Clauses(clause.OnConflict{DoNothing: true}).Create(&keywords)
+
 	pref := tele.Settings{
 		Token:  os.Getenv("TOKEN"),
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
@@ -18,13 +66,78 @@ func main() {
 
 	b, err := tele.NewBot(pref)
 	if err != nil {
-		log.Fatal(err)
-		return
+		rollbar.LogPanic(err, true)
+		panic(errors.New("failed to build bot"))
 	}
 
 	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
+		return c.Send("Hello")
+	})
+
+	b.Handle("/start", func(c tele.Context) error {
+		if c.Chat().Type != tele.ChatPrivate {
+			return nil
+		}
+
+		var user = User{Username: c.Sender().Username, TgID: c.Sender().ID}
+		result := db.First(&user, &user)
+		if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Send("You are already in the party!")
+		}
+		db.Unscoped().FirstOrCreate(&user, &user)
+		if user.DeletedAt.Valid {
+			db.Unscoped().Model(&user).Update("deleted_at", nil)
+		}
+		var keywords []Keyword
+		db.Find(&keywords)
+		err := db.Model(&user).Association("Keywords").Replace(&keywords)
+		if err != nil {
+			rollbar.Error(err)
+			return err
+		}
+
+		rollbar.Info("Added user: " + user.Username)
+		return c.Send("Hello! Now you will receive notifications as soon as a message with a hashtag appears.")
+	})
+
+	b.Handle("/stop", func(c tele.Context) error {
+		if c.Chat().Type != tele.ChatPrivate {
+			return nil
+		}
+
+		var user = User{Username: c.Sender().Username, TgID: c.Sender().ID}
+		db.Delete(&user, &user)
+
+		rollbar.Info("Deleted user: " + user.Username)
+		return c.Send("Bye. Notifications won't bother you anymore.")
+	})
+
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		if c.Chat().Type == tele.ChatPrivate {
+			return nil
+		}
+
+		var keywords []Keyword
+
+		err := db.Model(&Keyword{}).Preload("Users").Find(&keywords).Error
+		if err != nil {
+			rollbar.Error(err)
+		}
+
+		for _, keyword := range keywords {
+			if strings.Contains(c.Message().Text, keyword.Name) {
+				for _, user := range keyword.Users {
+					var recipient tele.Recipient = user
+					err := c.ForwardTo(recipient)
+					if err != nil {
+						rollbar.Warning(err)
+					}
+				}
+			}
+		}
+		return nil
 	})
 
 	b.Start()
+	rollbar.Wait()
 }
